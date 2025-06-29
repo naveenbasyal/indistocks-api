00b36da9039b89485f2513988e49ba5195271ea2
Naveen Basyal <naveenbasyal.001@gmail.com>
2025-06-30 02:26:12 +0530

    a

--- src/index.ts
+++ src/index.ts
@@ -14,17 +14,16 @@ const PORT = process.env.PORT || 8001;
 app.use(cors());
 
 app.use(express.json());
-app.use("/", (_, res) => {
-  res.status(200).json({
-    message: "Server Running",
-  });
-});
 
 app.use("/api/stocks", authMiddleware, validateApiKeyRedis, stocksRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/plans", plansRoutes);
 app.use("/api/payments", authMiddleware, paymentRoutes);
-
+app.use("/", (_, res) => {
+  res.status(200).json({
+    message: "Server Running",
+  });
+});
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
